5b14375cf605f6cc8a1f726d4a5fc5f4b2712151
120210094 LORENNA VICTORIA MARINHO LUCENA <lorenna.lucena@ccc.ufcg.edu.br>
2022-09-28 11:29:06 -0300

    psoft-aula1

---
+++ Lote.java
@@ -0,0 +1,13 @@
+public class Lote{
+
+    private int quantidade;
+    private String datav;
+    private Produto produto;
+
+    public Lote(int quantidade, String datav, Produto produto){
+        this.quantidade = quantidade;
+        this.datav = datav;
+        this.produto = produto;
+    }
+    
+}
---
+++ Sistema.java
@@ -0,0 +1,8 @@
+public class Sistema {
+
+    public static void main(String args[]){
+        Produto p = new Produto("Bike Lorenna", "Lorenna bike's ltda", 999.99);
+        Lote l = new Lote(9, "12/12/2023", p);
+    }
+    
+}
